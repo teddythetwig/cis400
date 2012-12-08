4d2fa1b75a7989a1bb6d5553c36e7c435dd6816c
dtrupenn <dtrupenn@gmail.com>
2012-12-07 20:00:35 -0500

    Initial js library

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,62 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+
+//List of tables
+tables = new Array();
+
+//Unlinked Properties
+props = new Array();
+
+//Table constructor
+function table(name){
+	if(uniqueTable(name)){
+		this.name = name;
+	} else{
+		return;
+	}
+	this.properties = new Array();
+	this.properties.push(name + "id");
+	this.square = new Rect(0, 0, 100, 100);
+	this.square.addTo(stage);d
+	this.square.fill('red');
+	
+	tables.push(this);	
+}
+
+//Property constructor
+function property(name){
+	this.name = name;
+	this.type = null;
+	props.push(this);
+}
+
+
+function uniqueTable(name){
+	for(var i = 0; i < tables.length; i++){
+		if(name.toLowerCase() == tables[i].name.toLowerCase()){
+			return false;
+		}
+	}
+	return true;
+}
+
+function uniqueProperty(table, name){
+	for(var i = 0; i < table.properties.length; i++){
+		if(table.properties[i].name == name){
+			return false;
+		}
+	}
+	return true;
+}
+
+function addPropToTable(table, propId){
+	var property= props[propId];
+	if(uniqueProperty(table, property.name)){
+		table.properties.push(property);
+		props.splice(propId, 1);
+	} else{
+		return false;
+	}
+}
+
