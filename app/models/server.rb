f0fe58236ddc03930858db0782082437abbd402c
Edward Funger <teddythetwig@gmail.com>
2013-01-10 12:45:41 -0500

    getter and setter methods for server db_json

--- app/models/server.rb
+++ app/models/server.rb
@@ -3,7 +3,8 @@ class Server < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
   
-  
+  # These two callbacks ensure that the application server is syncronized with the RDS server in terms
+  # of server existence
   after_create do |s|
     #initialize db on the rds servers
     #change this from hardcoding the password, on the first time it should set it
@@ -14,10 +15,25 @@ class Server < ActiveRecord::Base
   after_destroy do |s|
     $RDS.delete_db_instance(:db_instance_identifier => "#{name}", :skip_final_snapshot => true)
   end
-  
+
+  before_save do |s|
+    self[:db_json] ||= ("{\"tables\":[]}")
+  end
+  # Retrieves the server status from RDS server
   def check_status
     response = $RDS.describe_db_instances(:db_instance_identifier => "#{name}")
-    return response[:db_instances].first[:db_instance_status]
+    response = response[:db_instances].first[:db_instance_status]
+  end
+  
+  
+  # Json values are stored in the database as a string, these methods allow working 
+  # with json as an object without the intermediate step of conversion
+  def db_json
+    ActiveSupport::JSON.decode(self[:db_json])
+  end
+  
+  def db_json=(value)
+    self[:db_json] = ActiveSupport::JSON.encode(value)
   end
 
 end
