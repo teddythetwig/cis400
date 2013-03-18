8491fb9c070376de06eab31edce06cc77eb29213
dtrupenn <dtrupenn@gmail.com>
2013-03-18 00:57:31 -0400

    Fixed the jquery issue and added more stuff to convert method

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,6 @@ module CIS400
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib/assets)
-    config.autoload_paths += %W(#{config.root}/lib/assets)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
